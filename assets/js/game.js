82f439eb0086169fcc369252d7da726760430291
Victor <victor.a.paniagua.a@gmail.com>
2020-06-08 20:48:53 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,6 +80,14 @@ for(var i = 0; i < enemyNames.length; i++) {
     enemyHealth = 50;
     fight(pickedEnemyName);
 }
+
+  if (playerHealth >0 && i<enemyNames.length -1) {
+    var storeConfirm = window.confirm("This fight is over, visit the store before the next round?");
+    if (storeConfirm) {
+    shop();
+    }
+  }
+
   else{
     window.alert("You have lost your robot in battle! Game Over!");
     break;
@@ -111,6 +119,60 @@ var endGame = function() {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
 };
+
+var shop = function() {
+  var shopOptionPrompt = window.prompt (
+    "Would you like to REFILL your health, Upgrade your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+
+  switch (shopOptionPrompt) {
+    
+    case "REFILL": // new case
+    case "refill":
+      if (playerMoney >= 7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+
+      //increase health and decrease money
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney -7;
+      }
+      
+      else {
+        window.alert("You don't have enought money!");
+      }
+
+      break;
+
+    case "UPGRADE": // new case
+    case "upgrade":
+      if (playerMoney >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+      //increase attack and decrease money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enought money!");
+      }
+
+      break;
+
+    case "LEAVE": // new case
+    case "leave":
+      window.alert("Leaving the store.");
+
+      //do nothing, so the function will end
+      break;
+
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+
+      //call shop() again to force player to pick a valid option
+      shop();
+      break;
+  }
+}
 
 
 
